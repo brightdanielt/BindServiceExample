24f6ceba4e97a69dc0ef058f81236ca7b4b99614
danielt <brightdanielt@gmail.com>
2018-03-08 15:02:28 +0800

    完成可以被綁定的 Service

---
+++ app/src/main/java/com/example/danielt/bindserviceexample/MyService.java
@@ -0,0 +1,38 @@
+package com.example.danielt.bindserviceexample;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.Binder;
+import android.os.IBinder;
+import android.support.annotation.Nullable;
+import android.widget.Toast;
+
+/**
+ * Created by danielt on 2018/3/8.
+ */
+
+public class MyService extends Service {
+
+    MyService(){}
+
+    MyBinder binder = new MyBinder();
+
+    //符合規範的 Activity 成功啟動該 Service 時，透過該方法取得 MyBinder
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return binder;
+    }
+
+    //MyBinder 提供取得 MyService 的方法
+    class MyBinder extends Binder {
+        public Service getService() {
+            return MyService.this;
+        }
+
+    }
+
+    public void toast(){
+        Toast.makeText(MyService.this, "Method called from MyService", Toast.LENGTH_SHORT).show();
+    }
+}
